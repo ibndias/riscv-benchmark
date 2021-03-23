12220e1a1a041574ec44d3a4b7a0966391621f6f
Derry Pratama <derry@islab.re.kr>
2021-03-23 18:53:44 +0900

    remove PK env variable

--- env.sh
+++ env.sh
@@ -1,4 +1,5 @@
-export RISCV=/home/derry/riscv
-export PK=$RISCV/riscv64-unknown-elf/bin/pk
+export RISCV=/home/derry/Documents/Project/riscv-mte/install
+#export RISCV=/home/derry/riscv
+#export PK=$RISCV/riscv64-unknown-elf/bin/pk
 export PATH=$RISCV/bin:$PATH
 export RISCV_XLEN=64
